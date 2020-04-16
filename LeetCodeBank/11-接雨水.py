ffac3141db48618ad8bf0743f377b999de387568
LiChuang <15801760148@163.com>
2020-04-16 22:14:04 +0800

    第十一题、接雨水

---
+++ LeetCodeBank/11-接雨水.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python
+# coding: utf-8
+
+# LeetCode 题号 42
+
+
+# 给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。
+
+# 输入: [0,1,0,2,1,0,1,3,2,1,2,1]
+# 输出: 6
+
+
+# 使用双指针的方式计算
+class Solution:
+    def trap(self, height: list) -> int:
+        pass
+
+
+
+if __name__ == '__main__':
+    height = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
+    sol = Solution()
+    print(sol.trap(height))
